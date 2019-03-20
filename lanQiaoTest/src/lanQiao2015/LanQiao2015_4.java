ef1ed954c1de205d7036452cb95cd2a89dd26e80
HAODeChen <38579521+TheMoonDark@users.noreply.github.com>
2019-03-20 14:08:57 +0800

    Add files via upload

---
+++ lanQiaoTest/src/lanQiao2015/LanQiao2015_4.java
@@ -0,0 +1,26 @@
+import java.util.Vector;
+
+public class LanQiao2015_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(f(11, 13));
+	}
+	public static int f(int n, int m)
+	{
+		n = n % m;	
+		Vector v = new Vector();
+		
+		for(;;)
+		{
+			v.add(n);
+			n *= 10;
+			n = n % m;
+			if(n==0) 
+				return 0;
+			if(v.indexOf(n)>=0)  
+				return v.size() - v.indexOf(n);  //МоїХ
+		}
+	}
+
+}
